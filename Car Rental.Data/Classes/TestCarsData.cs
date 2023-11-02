a2d470fdcd99436dcfa6161afb137d0b12a80d92
kimpoliini <kimpas@hotmail.se>
2023-11-02 13:20:52 +0100

    Removed unused code

--- Car Rental.Data/Classes/TestCarsData.cs
+++ Car Rental.Data/Classes/TestCarsData.cs
@@ -2,11 +2,9 @@
 using Car_Rental.Common.Enums;
 using Car_Rental.Common.Interfaces;
 using Car_Rental.Data.Interfaces;
-using System.IO;
+using System.ComponentModel;
+using System.Data;
 using System.Net.Http.Json;
-using System.Net.NetworkInformation;
-using System.Text.Json;
-
 
 namespace Car_Rental.Data.Classes;
 
@@ -16,56 +14,41 @@ public class TestCarsData : IData
     List<IBooking> _bookings = new();
     List<IPerson> _persons = new();
 
+    public TestCarsData() => SeedData();
 
-    public TestCarsData()
-    {
-        SeedData();
-    }
-
+    //Creates vehicles, customers and bookings on initialization
     void SeedData()
     {
-        //"Not allowed to load local resource: file:///vehicles.json"
-        //HttpClient http = new();
-        //http.BaseAddress = new Uri("/sample-data");
-        //List<Car>? jsonVehicles = await http.GetFromJsonAsync<List<Car>>("vehicles.json");
-
-        List<Car> vehicles = new()
+        SetVehicles(new List<IVehicle>()
         {
             new Car("MOR334",VehicleMake.Volvo,VehicleType.StationWagon,340000,1.2,100),
             new Car("PMG44A",VehicleMake.Volkswagen,VehicleType.Hatchback,23000,1.2,100),
             new Car("NYO00M",VehicleMake.Koenigsegg,VehicleType.Sport,1200,5,1200),
-            new Car("ARG214",VehicleMake.Dukati,VehicleType.Motorcycle,10000,2.2,400),
+            new Motorcycle("ARG214",VehicleMake.Dukati,VehicleType.Motorcycle,10000,2.2,400),
             new Car("SHR445",VehicleMake.Ford,VehicleType.Hatchback,25000,1,80),
             new Car("PJL392",VehicleMake.Polestar,VehicleType.Sedan,8000,0.8,100),
-        };
+        });
 
-        SetVehicles(vehicles);
-
-    }
-    public IEnumerable<IVehicle> GetVehicles() => _vehicles;
-    public void SetVehicles(IEnumerable<IVehicle> vehicles)
-    {
-
-        _vehicles.AddRange(vehicles);
-
-        //Populate bookings and persons with fake data when vehicles has been added
-        List<Customer> customers = new()
+        SetCustomers(new List<Customer>
         {
             new Customer(1, "Kim", "Hellman"),
             new Customer(12, "Jim", "Kellman"),
             new Customer(8901, "Lim", "Snellman"),
-        };
-        SetCustomers(customers);
+        });
 
-        List<Booking> bookings = new()
+        SetBookings(new List<Booking>
         {
             new Booking(GetVehicles().ElementAt(0), GetCustomers().ElementAt(0), 200),
             new Booking(GetVehicles().ElementAt(3), GetCustomers().ElementAt(2), 400),
             new Booking(GetVehicles().ElementAt(4), GetCustomers().ElementAt(1), 600, 600,
             DateTime.Parse("2023-09-22"), DateTime.Parse("2023-09-27"))
-        };
-        SetBookings(bookings);
+        });
     }
+
+    public IEnumerable<IVehicle> GetVehicles() => _vehicles;
+
+    public void SetVehicles(IEnumerable<IVehicle> vehicles) => _vehicles.AddRange(vehicles);
+
 
     public IEnumerable<IBooking> GetBookings() => _bookings;
     public void SetBookings(IEnumerable<IBooking> bookings)
--- CarRental.App/Program.cs
+++ CarRental.App/Program.cs
@@ -10,7 +10,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddSingleton<IData, TestCarsData>();
-builder.Services.AddSingleton<Business>();
+builder.Services.AddScoped<IData, TestCarsData>();
+builder.Services.AddScoped<Business>();
 
 await builder.Build().RunAsync();
